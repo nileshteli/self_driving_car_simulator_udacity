e9eb9f0d5ddac67fb30b465fe47a5826b516e920
Mongoose <147689270+nileshteli@users.noreply.github.com>
2026-05-08 16:46:20 +0530

    Add files via upload

---
+++ drive.py
@@ -0,0 +1,69 @@
+import socketio
+import eventlet
+import numpy as np
+from flask import Flask
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense
+ 
+sio = socketio.Server()
+ 
+app = Flask(__name__)
+speed_limit = 10
+
+def build_model():
+    model = Sequential([
+        Conv2D(24, (5, 5), strides=(2, 2), activation='elu', input_shape=(66, 200, 3)),
+        Conv2D(36, (5, 5), strides=(2, 2), activation='elu'),
+        Conv2D(48, (5, 5), strides=(2, 2), activation='elu'),
+        Conv2D(64, (3, 3), activation='elu'),
+        Flatten(),
+        Dense(100, activation='elu'),
+        Dense(50, activation='elu'),
+        Dense(10, activation='elu'),
+        Dense(1)
+    ])
+    return model
+
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+ 
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image, verbose=0))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
+ 
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    send_control(0, 0)
+ 
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data = {
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
+    })
+ 
+if __name__ == '__main__':
+    model = build_model()
+    model.load_weights('modelnew.h5', by_name=True, skip_mismatch=True)
+
+    print("Model loaded successfully!")
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
